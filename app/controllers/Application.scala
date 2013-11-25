5be841197daaa96a85b29b7e79ddf6263aa96b7e
thibault <thibault@thibault-VirtualBox.(none)>
2013-11-25 17:29:00 +0000

    Templates

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,21 @@
 package controllers
 
+import anorm._
 import play.api._
+import play.api.Play.current 
 import play.api.mvc._
+import play.api.db._
 
 object Application extends Controller {
-
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+	def index = Action { implicit request =>
+		println(request.getQueryString("foo"))
+		DB.withConnection { implicit c =>
+			val result = SQL("Select * from places")
+			val show = result().map(row => 
+				row[String]("X_place") -> row[String]("Y_place")
+			).toList
+			Ok(views.html.index(show))
+		} 
+	}
 
 }
